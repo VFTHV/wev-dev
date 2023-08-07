6fa7cb5a1f4a24c50e6436625b66f13851059a29
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-06 22:09:21 -0500

    JSX setup for Home and Technologies

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import {
+  Logo,
+  JsxTag,
+  NeuronIcon,
+  ReactTech,
+  TypeScripTech,
+  NodeTech,
+} from './components/SVGs';
+import { Squash as Hamburger } from 'hamburger-react';
+import { HiOutlineCubeTransparent } from 'react-icons/hi';
+import { BsArrowDown } from 'react-icons/bs';
+import { GoGlobe } from 'react-icons/go';
+import { SiRedux } from 'react-icons/si';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="hamburger">
+          <Hamburger color="rgb(9, 232, 94)" />
+        </div>
+      </header>
+      <section className="home">
+        <div className="hero-img">
+          <img src="avatar.png" alt="web developer image" />
+        </div>
+        <h1 className="h1">
+          I'm a wev developer based in Sugar Land, Tx, specializing in
+          React+Redux
+        </h1>
+        <div className="cube">
+          <HiOutlineCubeTransparent />
+        </div>
+        <div className="globe">
+          <GoGlobe />
+        </div>
+        <div className="jsx-tag">
+          <JsxTag />
+        </div>
+        <div className="neuron">
+          <NeuronIcon />
+        </div>
+        <div className="scroll-down">Scroll Down</div>
+        <div className="down">
+          <BsArrowDown />
+        </div>
+      </section>
+      <section className="technologies">
+        <h2 className="h2">Technologies I work with</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          provident esse distinctio, est minima quidem dolor laboriosam maxime
+          reprehenderit autem.
+        </p>
+        <div className="techs">
+          <div className="tech">
+            <ReactTech />
+          </div>
+          <div className="tech">
+            <TypeScripTech />
+          </div>
+          <div className="tech">
+            <NodeTech />
+          </div>
+          <div className="tech">
+            <SiRedux />
+          </div>
+        </div>
+      </section>
+      <section className="projects">
+        <h2 className="h2">Selected Projects</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
