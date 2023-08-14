899027facf2beec5a8562a1e746bfa22b43dc5ec
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-13 22:34:17 -0500

    FAQ.tsx styled

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,19 +1,7 @@
-import { useState, useRef } from 'react';
-import { motion, useTransform, useScroll } from 'framer-motion';
-import { BiDownArrow } from 'react-icons/bi';
 import '../styles/faqs.css';
+import FAQ from './FAQ';
 
 function FAQs() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleOpen = () => setIsOpen((prev) => !prev);
-
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start end', 'start'],
-  });
-  const width = useTransform(scrollYProgress, [0.1, 0.5], ['0%', '100%']);
-
   return (
     <section className="faqs container">
       <h2 className="h2">FAQs</h2>
@@ -22,37 +10,24 @@ function FAQs() {
         nobis reprehenderit a aspernatur dolores eveniet quis eligendi?
         Praesentium, enim?
       </p>
-      <div className="question-wrapper">
-        <div className="question">
-          <h3 className="h3">What techonoly stack do you use</h3>
-          <motion.div
-            animate={{
-              transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-            }}
-            onClick={toggleOpen}
-            className="arrow-wrapper"
-          >
-            <BiDownArrow />
-          </motion.div>
-          <motion.div
-            ref={ref}
-            style={{ width }}
-            className="border-top"
-          ></motion.div>
-        </div>
-        <div className="answer-wrapper">
-          <motion.div
-            animate={{
-              height: isOpen ? 'min-content' : 0,
-            }}
-            className="answer descr"
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-            laudantium esse tempora reprehenderit! Odit nobis cupiditate soluta
-            totam corporis. Cum?
-          </motion.div>
-        </div>
-      </div>
+      <FAQ questionText="What techonology stack do you use?">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse
+          nisi quasi ullam, sed ipsa dolorem eaque voluptatibus cum quibusdam?
+        </p>
+      </FAQ>
+      <FAQ questionText="What is your experience level?">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint error
+          totam autem rerum tempora!
+        </p>
+      </FAQ>
+      <FAQ questionText="What is your pricing?">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
+          pariatur.
+        </p>
+      </FAQ>
     </section>
   );
 }
