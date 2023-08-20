f780d9f167a4b8180a25728b88fb27b5d4c57238
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-20 15:06:34 -0500

    Styled Carousel.tsx sizes

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -17,50 +17,16 @@ function Carousel() {
   }, []);
 
   return (
-    <motion.div
-      ref={carousel}
-      style={{
-        cursor: 'grab',
-        width: '100%',
-        overflow: 'hidden',
-        // backgroundColor: 'pink',
-      }}
-      // className='carousel'
-    >
+    <motion.div ref={carousel} className="carousel">
       <motion.div
         drag="x"
         dragConstraints={{ right: 0, left: width }}
-        // className="inner-carousel"
-        style={{
-          // backgroundColor: 'pink',
-          display: 'flex',
-          width: 'max-content',
-          padding: 0,
-          margin: 0,
-        }}
+        className="inner-carousel"
       >
         {images.map((image, index) => {
           return (
-            <motion.div
-              // className="item"
-              key={index}
-              style={{
-                height: '5rem',
-                padding: '1rem',
-                // backgroundColor: 'red',
-                margin: '5px',
-              }}
-            >
-              <img
-                style={{
-                  height: '100%',
-                  width: '100%',
-                  objectFit: 'cover',
-                  pointerEvents: 'none',
-                }}
-                src={image}
-                alt="partners image"
-              />
+            <motion.div className="item" key={index}>
+              <img src={image} alt="partners image" />
             </motion.div>
           );
         })}
