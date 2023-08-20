4dcc85ee5bab273d2056a35f05fe9d81b64b4f3c
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-20 00:33:55 -0500

    Carousel basic functionality

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,16 +2,34 @@ import { motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import images from '../assets/images';
 import '../styles/carousel.css';
+import { nanoid } from 'nanoid';
 
 function Carousel() {
-  console.log(images);
+  const [width, setWidth] = useState(0);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    if (carousel.current) {
+      // setTimeout(() => {
+      const { offsetWidth, scrollWidth } = carousel.current;
+      console.log(offsetWidth, scrollWidth);
+      const newWidth = offsetWidth - scrollWidth;
+      setWidth(newWidth);
+      // }, 200);
+    }
+  }, []);
+
   return (
-    <motion.div className="carousel">
-      <motion.div className="inner-carousel">
+    <motion.div ref={carousel} className="carousel">
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0, left: width }}
+        className="inner-carousel"
+      >
         {images.map((image) => {
           return (
-            <motion.div>
-              <img src={image} alt="" />
+            <motion.div className="item" key={nanoid()}>
+              <img src={image} alt="partners image" />
             </motion.div>
           );
         })}
