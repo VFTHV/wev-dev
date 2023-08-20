d8fefaf0148b5e23af5b08379c5ea702469f62a2
Vadim Fattakhov <vadfthv@gmail.com>
2023-08-20 13:32:10 -0500

    Styled draggable carousel

--- src/assets/images.ts
+++
@@ -1,7 +0,0 @@
-import image1 from '/bh-logo.png';
-import image2 from '/tgt-logo.png';
-import image3 from '/ginnovo-logo.png';
-import image4 from '/ahws-logo.png';
-import image5 from '/klx-logo.png';
-
-export default [image1, image2, image3, image4, image5];
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,35 +1,66 @@
 import { motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
-import images from '../assets/images';
 import '../styles/carousel.css';
-import { nanoid } from 'nanoid';
 
 function Carousel() {
+  const images = ['/bh2.png', '/tgt.png', 'ginnovo.png', 'ahws.png', 'klx.png'];
   const [width, setWidth] = useState(0);
   const carousel = useRef(null);
 
   useEffect(() => {
     if (carousel.current) {
-      // setTimeout(() => {
       const { offsetWidth, scrollWidth } = carousel.current;
       console.log(offsetWidth, scrollWidth);
       const newWidth = offsetWidth - scrollWidth;
       setWidth(newWidth);
-      // }, 200);
     }
   }, []);
 
   return (
-    <motion.div ref={carousel} className="carousel">
+    <motion.div
+      ref={carousel}
+      style={{
+        cursor: 'grab',
+        width: '100%',
+        overflow: 'hidden',
+        // backgroundColor: 'pink',
+      }}
+      // className='carousel'
+    >
       <motion.div
         drag="x"
         dragConstraints={{ right: 0, left: width }}
-        className="inner-carousel"
+        // className="inner-carousel"
+        style={{
+          // backgroundColor: 'pink',
+          display: 'flex',
+          width: 'max-content',
+          padding: 0,
+          margin: 0,
+        }}
       >
-        {images.map((image) => {
+        {images.map((image, index) => {
           return (
-            <motion.div className="item" key={nanoid()}>
-              <img src={image} alt="partners image" />
+            <motion.div
+              // className="item"
+              key={index}
+              style={{
+                height: '5rem',
+                padding: '1rem',
+                // backgroundColor: 'red',
+                margin: '5px',
+              }}
+            >
+              <img
+                style={{
+                  height: '100%',
+                  width: '100%',
+                  objectFit: 'cover',
+                  pointerEvents: 'none',
+                }}
+                src={image}
+                alt="partners image"
+              />
             </motion.div>
           );
         })}
