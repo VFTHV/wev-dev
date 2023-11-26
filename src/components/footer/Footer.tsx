4e56338e951558a10a1e32a76e455922d0db909b
Vadim Fattakhov <vadfthv@gmail.com>
2023-11-27 08:32:02 +0930

    Extra content for footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,8 @@ import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
 import './footer.css';
 
+// EXTRA CONTENT FOR FOOTER
+
 function Footer() {
   return (
     <footer className="footer container">
