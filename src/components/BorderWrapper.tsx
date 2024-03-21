da9ecc5d92939131d21fd1ab4ff852f3dac2de37
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-20 22:22:52 -0500

    improve border wrapper

--- src/components/BorderWrapper.tsx
+++ src/components/BorderWrapper.tsx
@@ -8,6 +8,7 @@ interface BorderWrapperProps {
 
 // improve border wrapper
 // improve border wrapper
+// improve border wrapper
 
 function BorderWrapper({ children, transformEnd }: BorderWrapperProps) {
   const horizBorderRef = useRef<HTMLDivElement>(null);
